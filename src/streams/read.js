6bf617cbcd2ecd8e9842d1bc35cc4e81f0d61ab4
Mechyslau Vakaliuk <Mechyslau_Vakaliuk@epam.com>
2022-06-03 11:59:19 +0300

    feat: implement all functionality on the streams folder

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,25 @@
+import { createReadStream } from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+
 export const read = async () => {
-    // Write your code here 
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+  try {
+    const content = createReadStream(
+      path.join(__dirname, "files", "fileToRead.txt"),
+      { encoding: "utf8" }
+    );
+    content.on("data", (data) => {
+      process.stdout.write(data);
+    });
+
+    content.on("error", (error) => {
+      throw error;
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
 };
+
+read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,24 @@
+import { createWriteStream } from "fs";
+import { fileURLToPath } from "url";
+import path from "path";
+
 export const write = async () => {
-    // Write your code here 
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+  try {
+    const writeStream = createWriteStream(
+      path.join(__dirname, "files", "fileToWrite.txt")
+    );
+    process.stdin.on("data", (data) => {
+      writeStream.write(data);
+    });
+
+    process.stdin.on("error", (error) => {
+      throw error;
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
 };
+
+write();
