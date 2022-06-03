708803032f21bdfe29d7bd82c8fd5c8b3314a2bc
Mechyslau Vakaliuk <Mechyslau_Vakaliuk@epam.com>
2022-06-03 14:55:44 +0300

    feat: implement all functions on the zip folder

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,52 @@
+import { createReadStream, createWriteStream } from "fs";
+import { rm, lstat } from "fs/promises";
+import { pipeline } from "stream";
+import { fileURLToPath } from "url";
+import path from "path";
+import zlib from "zlib";
+
 export const compress = async () => {
-    // Write your code here 
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+  try {
+    const file = await lstat(path.join(__dirname, "files", "archive.gz"));
+    if (file.isFile()) {
+      throw new Error("The archive is already created");
+    }
+  } catch (error) {
+    if (error.code !== "ENOENT") throw error;
+  }
+
+  try {
+    await lstat(path.join(__dirname, "files", "fileToCompress.txt"));
+  } catch (error) {
+    if (error.code === "ENOENT")
+      throw new Error("The fileToCompress is not found");
+  }
+
+  const readStream = createReadStream(
+    path.join(__dirname, "files", "fileToCompress.txt")
+  );
+  const zip = zlib.createGzip();
+  const writeStream = createWriteStream(
+    path.join(__dirname, "files", "archive.gz")
+  );
+
+  const deleteOriginFile = async () => {
+    try {
+      await rm(path.join(__dirname, "files", "fileToCompress.txt"));
+    } catch (error) {
+      throw new Error(error.code);
+    }
+  };
+
+  writeStream.on("finish", () => {
+    deleteOriginFile();
+  });
+
+  pipeline(readStream, zip, writeStream, (error) => {
+    if (error) throw new Error(error);
+  });
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,53 @@
+import { createReadStream, createWriteStream } from "fs";
+import { lstat, rm } from "fs/promises";
+import { pipeline } from "stream";
+import { fileURLToPath } from "url";
+import path from "path";
+import zlib from "zlib";
+
 export const decompress = async () => {
-    // Write your code here 
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+  try {
+    await lstat(path.join(__dirname, "files", "archive.gz"));
+  } catch (error) {
+    if (error.code === "ENOENT") throw new Error("The zippedFile is not found");
+  }
+
+  try {
+    const unZippedFile = await lstat(
+      path.join(__dirname, "files", "fileToCompress.txt")
+    );
+    if (unZippedFile.isFile()) {
+      throw new Error("The fileToCompress.txt is already exists");
+    }
+  } catch (error) {
+    if (error.code !== "ENOENT") throw error;
+  }
+
+  const readStream = createReadStream(
+    path.join(__dirname, "files", "archive.gz")
+  );
+  const upZip = zlib.createUnzip();
+  const writeStream = createWriteStream(
+    path.join(__dirname, "files", "fileToCompress.txt")
+  );
+
+  const deleteGzipFile = async () => {
+    try {
+      await rm(path.join(__dirname, "files", "archive.gz"));
+    } catch (error) {
+      throw new Error(error.code);
+    }
+  };
+
+  writeStream.on("finish", () => {
+    deleteGzipFile();
+  });
+
+  pipeline(readStream, upZip, writeStream, (error) => {
+    if (error) throw new Error(`Error: ${error}`);
+  });
 };
+
+decompress();
