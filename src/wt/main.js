5a2b015f69421c58e8751159c93900b0e4630cbe
Mechyslau Vakaliuk <Mechyslau_Vakaliuk@epam.com>
2022-06-04 17:07:14 +0300

    feat: implement functionality on the wt folder

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,35 @@
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+import { fileURLToPath } from "url";
+import path from "path";
+
 export const performCalculations = async () => {
-    // Write your code here
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+  const INITIAL_VALUE = 10;
+  const results = [];
+
+  const workers = new Array(cpus().length).fill(0).map(
+    (worker, ind) =>
+      (worker = new Worker(path.join(__dirname, "worker.js"), {
+        workerData: INITIAL_VALUE + ind,
+      }))
+  );
+
+  const getWorkersResult = () =>
+    new Array(workers.length).fill(0).map((item, workerInd) => {
+      item = new Promise((res, rej) => {
+        workers[workerInd].on("message", res);
+        workers[workerInd].on("error", rej);
+      });
+      return item;
+    });
+
+  for await (let worker of getWorkersResult()) {
+    results.push(worker);
+  }
+
+  console.log(results);
 };
+
+performCalculations();
