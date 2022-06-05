9e800bbf49dc37aa973e24f3419e63f3b5135ef6
Mechyslau Vakaliuk <Mechyslau_Vakaliuk@epam.com>
2022-06-05 14:46:56 +0300

    init: add command scripts

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,7 +1,7 @@
 export const parseArgs = () => {
   const result = process.argv.splice(2)
     .map((item, ind, arr) => {
-      if (ind % 2 === 0) return `${item.replace(/^--/, '')} is ${arr[++ind]}`;
+      if (ind % 2 === 0) return `${item.replace(/^(--)/, '')} is ${arr[++ind]}`;
     })
     .filter(item => item)
     .join(", ");
